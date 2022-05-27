3450566e243d0d92d81da7a17dfa73f1dbbce068
Nikos Typaldos <nmtypaldos@yahoo.com>
2022-05-27 16:15:35 +0300

    Fixed Hamburger Button

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const hamburger = document.querySelectorAll(".hamburger");
+const hamburgerBtn = document.querySelector(".demo");
 const navbarItem = document.querySelectorAll(".nav-link");
 const linksContainer = document.querySelector(".links-container");
 const navbar = document.querySelector(".navbar");
 
-
-
 function toggleMenu() {
     if(linksContainer.classList.contains("show")) {
         linksContainer.classList.remove("show");
-        navbar.classList.remove("navShow");
     } else {
         linksContainer.classList.add("show");
-        navbar.classList.add("navShow");
     }
-
-    
 }
 
-hamburger[0].addEventListener("click", toggleMenu)
+hamburgerBtn.addEventListener("click", toggleMenu)
