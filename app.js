bf9ab6f56fc816f89f2f057655cb3eeef5dec48d
Nikos Typaldos <nmtypaldos@yahoo.com>
2022-06-14 01:22:29 +0300

    added car rental cost page

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ const hamburgerBtn = document.querySelector(".demo");
 const linksContainer = document.querySelector(".links-container");
 const navbar = document.querySelector(".navbar");
 
-
 function toggleMenu() {
-    if(linksContainer.classList.contains("show")) {
-        linksContainer.classList.remove("show");
-    } else {
-        linksContainer.classList.add("show");
-    }
+  if (linksContainer.classList.contains("show")) {
+    linksContainer.classList.remove("show");
+  } else {
+    linksContainer.classList.add("show");
+  }
 }
 
 window.onscroll = () => {
-    if (document.body.scrollTop >= navbar.offsetHeight || document.documentElement.scrollTop >= navbar.offsetHeight) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
+  if (
+    document.body.scrollTop >= navbar.offsetHeight ||
+    document.documentElement.scrollTop >= navbar.offsetHeight
+  ) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 };
 
 hamburgerBtn.addEventListener("click", toggleMenu);
