387d3c35ffebf65908773b73e76a34667a81b598
Nikos Typaldos <nmtypaldos@yahoo.com>
2022-06-17 14:50:31 +0300

    changes

--- rentalCost.js
+++ rentalCost.js
@@ -2,20 +2,21 @@ const btn= document.querySelector('#calcCost');
 const displayCost = document.querySelector('#displayCost');
 const calcCost = {
     cubism: document.querySelector('#engineDisplacement'),
-    driverAge: document.querySelector('#selectAge').value,
+    driverAge: document.querySelector('#selectAge'),
     pickupDate: document.querySelector('#fromDate'),
     dropOffDate: document.querySelector('#toDate')
 }
 
 btn.onclick = () => {
-    const premiumCost = 0;
-    const cost2 = 0;
-    var daysOfRental = (calcCost.dropOffDate.valueAsDate - calcCost.pickupDate.valueAsDate) / (1000 * 3600 * 24);
-    var ageCost = calcCost['driverAge'] >= 18 && calcCost['driverAge'] < 22 ? (calcCost['driverAge'] * 0.00042)
-                : calcCost['driverAge'] >= 22 && calcCost['driverAge'] < 30 ? (calcCost['driverAge'] * 0.00036)
-                : calcCost['driverAge'] >= 30 ? (calcCost['driverAge'] * 0.00023)
+    var premiumCost = 0;
+    var cost2 = 0;
+    
+    const daysOfRental = (calcCost.dropOffDate.valueAsDate - calcCost.pickupDate.valueAsDate) / (1000 * 3600 * 24);
+    const ageCost = calcCost['driverAge'].value >= 18 && calcCost['driverAge'].value < 22 ? (calcCost['driverAge'].value * 0.00042)
+                : calcCost['driverAge'].value >= 22 && calcCost['driverAge'].value < 30 ? (calcCost['driverAge'].value * 0.00036)
+                : calcCost['driverAge'].value >= 30 && calcCost['driverAge'].value <= 85? (calcCost['driverAge'].value * 0.00023)
                 : resetForm();
-    var cubismCostperDay = calcCost['cubism'].value <= 1600 ? 
+    const cubismCostperDay = calcCost['cubism'].value <= 1600 && calcCost['cubism'].value > 100? 
                                                         ( daysOfRental <= 5 && daysOfRental > 0 ? 15.42
                                                         : daysOfRental <= 10 && daysOfRental > 0? 14.62 
                                                         : daysOfRental > 10 && daysOfRental > 0? 13.15
@@ -30,7 +31,7 @@ btn.onclick = () => {
                                                             : daysOfRental <= 10 && daysOfRental > 0 ? 17.10 
                                                             : daysOfRental > 10 && daysOfRental > 0 ? 16.20
                                                             : resetForm()) 
-                        :  resetForm();
+                        :  console.log(15);
     premiumCost = ageCost * calcCost['cubism'].value;
     cost2 = daysOfRental * cubismCostperDay;
     displayCost.textContent = `${(premiumCost + cost2).toFixed(2)} €`;
@@ -41,10 +42,3 @@ function resetForm() {
     displayCost.textContent = 0 +' €';
     document.getElementById('costForm').reset();
 }
-
-
-
-
-
-
-
